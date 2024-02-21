b6add707295faffa5d9394a8801d5e1a77715589
David Questa <david.questa@pysae.com>
2024-02-21 22:54:16 +0100

    feat: header creation

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,5 +1,10 @@
 <template>
-    <div class="home">
+    <div class="home flex flex-col w-full items-center">
+        <div class="header h-24 w-[80%] flex justify-center items-center space-x-6 relative">
+            <h1 class="text-4xl text-left text-green-800 text-shadow-lg shadow-green-800 max-w-4/12 absolute left-4">Hubert-Miam</h1>
+            <input type="text" name="" id="" class="w-6/12 rounded-lg bg-gray-100 pl-2 text-sm h-8" placeholder="Rechercher un restaurant">
+            <button class="bg-green-800 text-white rounded-lg p-2 w-2/12 absolute right-4 shadow-green-800 hover:shadow-lg">Login</button>
+        </div>
         <RestaurantRow v-for="(row, index) in restaurantData" :key="index" :threeRestaurants="row"/>
     </div>
 </template>
