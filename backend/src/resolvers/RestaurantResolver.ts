80c82f306506367c907ab9c5694f08326b886bbe
Pouish85 <pouish.iphone@gmail.com>
2024-07-02 08:47:04 +0200

    Feat: finish CRUD and update CI

--- backend/src/resolvers/RestaurantResolver.ts
+++ backend/src/resolvers/RestaurantResolver.ts
@@ -1,7 +1,6 @@
 import {Resolver, Query, Mutation, Arg} from 'type-graphql';
 import { Restaurant } from '../entities/Restaurant';
 import { Country} from '../entities/Country';
-import DataSource from "../db";
 
 @Resolver(Restaurant)
 export default class RestaurantResolver {
@@ -39,6 +38,36 @@ export default class RestaurantResolver {
         });
         return Restaurant.save(restaurant);
         }
+
+    // Update restaurant Mutation
+    @Mutation(() => Restaurant)
+    async updateRestaurant(
+        @Arg("id") id: number,
+        @Arg("restaurantName") restaurantName: string,
+        @Arg("restaurantImage") restaurantImage: string,
+        @Arg("restaurantNote") restaurantNote: number,
+        @Arg("deliveryTimeMin") deliveryTimeMin: number,
+        @Arg("deliveryTimeMax") deliveryTimeMax: number,
+        @Arg("priceRate") priceRate: number,
+        @Arg("restaurantFoodType") restaurantFoodTypeId: number
+    ) {
+        const restaurant = await Restaurant.findOne({ where: { id } });
+        if (!restaurant) {
+            throw new Error("Restaurant not found!");
+        }
+        const restaurantFoodType = await Country.findOne({ where: { id: restaurantFoodTypeId } });
+        if (!restaurantFoodType) {
+            throw new Error("Restaurant food type not found");
+        }
+        restaurant.restaurantName = restaurantName;
+        restaurant.restaurantImage = restaurantImage;
+        restaurant.restaurantNote = restaurantNote;
+        restaurant.deliveryTimeMin = deliveryTimeMin;
+        restaurant.deliveryTimeMax = deliveryTimeMax;
+        restaurant.priceRate = priceRate;
+        restaurant.restaurantFoodType = restaurantFoodType;
+        return Restaurant.save(restaurant);
+    }
 
     // Delete restaurant Mutation
     @Mutation(() => Boolean)
